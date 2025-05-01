d8e5c4d1ccc2a5c211e94c5ecd5c3c558b4b0725
Your Name <ty510460@gamil.com>
2025-04-30 22:28:00 -0700

    added footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,94 +1,96 @@
 // components/Footer.jsx
 import {
-    Box,
-    Container,
-    Stack,
-    Text,
-    Link,
-    Icon,
-    SimpleGrid,
-    Heading,
-  } from "@chakra-ui/react"
-  import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa"
-  
-  // Reusable link section data
-  const footerSections = [
-    {
-      heading: "Quick Links",
-      links: ["Home", "Destinations", "Travel", "Culture", "Contact"],
-    },
-    {
-      heading: "Services",
-      links: ["Cultural Tours",  "Visa Assistance", "Travel Insurance"],
-    },
-  ];
-  
-  const socialLinks = [
-    { label: "Facebook", icon: FaFacebook, href: "#" },
-    { label: "Instagram", icon: FaInstagram, href: "#" },
-    { label: "whatsApp", icon: FaWhatsapp, href: "#" },
-  ]
-  
-  const Footer = () => {
-    return (
-      <Box bg="green" color="white">
-        <Container maxW="6xl" py={10}>
-          <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} wordSpacing={8}>
-            {/* Branding */}
-            <Stack wordSpacing={3}>
-              <Heading fontSize="lg" color="yellow">
-                Explore Bhutan
+  Box,
+  Container,
+  Stack,
+  Text,
+  Icon,
+  SimpleGrid,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const footerSections = [
+  {
+    heading: "Services",
+    links: ["Cultural Tours", "Visa Assistance", "Travel Insurance"],
+  },
+];
+
+const socialLinks = [
+  { label: "Facebook", icon: FaFacebook, href: "https://www.facebook.com/" },
+  { label: "Instagram", icon: FaInstagram, href: "https://www.instagram.com/" },
+  { label: "Youtube", icon: FaYoutube, href: "https://www.youtube.com/" },
+];
+
+const Footer = () => {
+  return (
+    <Box bg="green" color="white">
+      <Container maxW="6xl" py={10}>
+        <SimpleGrid columns={{ base: 1, md: 3 }} spaceX={10}>
+          {/* Logo & Branding */}
+          <Stack align="center">
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/5/5d/Emblem_of_the_Prime_Minister_of_Bhutan.png"
+              alt="Logo"
+              boxSize="100px"
+              borderRadius="full"
+              mb={2}
+            />
+            <Heading size="md">Explore Bhutan</Heading>
+            <Text fontSize="sm" textAlign="center">
+              Your gateway to the land of the Thunder Dragon
+            </Text>
+          </Stack>
+
+          {/* Link Sections */}
+          {footerSections.map(({ heading, links }) => (
+            <Stack key={heading} align={{ base: "center", md: "flex-start" }}>
+              <Heading size="sm" mb={2}>
+                {heading}
               </Heading>
-              <Text fontSize="sm">Discover more about Bhutan.</Text>
-            </Stack>
-  
-            {/* Dynamic Link Sections */}
-            {footerSections.map(({ heading, links }) => (
-              <Stack key={heading} align="flex-start" wordSpacing={1}>
-                <Text fontWeight="600" mb={2}>
-                  {heading}
-                </Text>
-                {links.map((link) => (
-                  <Link key={link} href="#" _hover={{ textDecoration: "underline" }}>
+              {links.map((link) => (
+                <Link key={link} to="#" style={{ textDecoration: "none" }}>
+                  <Text fontSize="sm" _hover={{ textDecoration: "underline" }}>
                     {link}
-                  </Link>
-                ))}
-              </Stack>
-            ))}
-  
-            {/* Contact & Social */}
-            <Stack align="flex-start" wordSpacing={1}>
-              <Text fontWeight="600" mb={2}>
-                Contact
-              </Text>
-              <Text fontSize="sm">Thimphu, Bhutan</Text>
-              <Text fontSize="sm">@explorebhutan.com</Text>
-              <Text fontSize="sm">+975 17538599 / 77312115</Text>
-              <Stack direction="row" wordSpacing={3} mt={2}>
-                {socialLinks.map(({ label, icon, href }) => (
-                  <Link
-                    key={label}
-                    href={href}
-                    aria-label={label}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Icon as={icon} boxSize={5} color="black" />
-                  </Link>
-                ))}
-              </Stack>
+                  </Text>
+                </Link>
+              ))}
             </Stack>
-          </SimpleGrid>
-        </Container>
-  
-        {/* Footer Bottom */}
-        <Box py={4}>
-          <Text fontSize="sm" textAlign="center">
-            &copy; {new Date().getFullYear()} Explore Bhutan. All rights reserved.
-          </Text>
-        </Box>
+          ))}
+
+          {/* Contact & Social */}
+          <Stack align={{ base: "center", md: "flex-start" }}>
+            <Heading size="sm" mb={2}>
+              Contact
+            </Heading>
+            <Text fontSize="sm">Thimphu, Bhutan</Text>
+            <Text fontSize="sm">info@explorebhutan.com</Text>
+            <Text fontSize="sm">+975 17538599 / 77312115</Text>
+            <Stack direction="row" spaceX={4} mt={3}>
+              {socialLinks.map(({ label, icon, href }) => (
+                <Link
+                  key={label}
+                  to={href}
+                  aria-label={label}
+                >
+                  <Icon as={icon} boxSize={6} _hover={{ color: "green.400" }} />
+                </Link>
+              ))}
+            </Stack>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+
+      <Box py={4}>
+        <Text fontSize="sm" textAlign="center">
+          &copy; 2025 <strong>Explore Bhutan</strong>. All rights reserved.
+        </Text>
       </Box>
-    )
-  }
-  
-  export default Footer
+    </Box>
+  );
+};
+
+export default Footer;
