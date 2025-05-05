e0d0e327ca234e2f5b02bdcde884b5f91de381ba
rigyel-dev <rigyel.ghalley@gmail.com>
2025-05-05 11:11:42 +0600

    error fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import './index.css'
 import { Box } from "@chakra-ui/react"
 import Footer from "./components/Footer"
 
-function App() {
+const App=()=> {
   return (
     <Box minH="100vh" display="flex" flexDirection="column">
       <NavBar />
 
-      {/* Main content takes remaining space */}
+      
       <Box flex="1">
         <AppRoutes />
       </Box>
 
-      {/* Footer stays at the bottom */}
+     
       <Footer />
     </Box>
   )
